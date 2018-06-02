4d2cf15287d49c9092f1bda429c152538365625f
amilonidis <alex.milo@outlook.com>
2018-06-02 10:45:01 +0300

    sharksite

---
+++ Shark/SharkSite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharkSite.Startup))]
+namespace SharkSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
