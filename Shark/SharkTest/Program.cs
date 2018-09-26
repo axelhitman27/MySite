ed746390c7a99ac90f8807c12f2f8e538456dd59
amilonidis <alex.milo@outlook.com>
2018-09-26 14:53:06 +0300

    shark testing

---
+++ Shark/SharkTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SharkTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Shark/SharkTesting/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharkTesting
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethodFirst()
+        {
+        }
+    }
+}
